4cdd2b37a5017ad55b9e99ce0f2795b87e1743ef
utham2509 <utham2509@gmail.com>
2026-07-15 19:22:37 -0400

    files updated

--- acts/rl/Gaussian.sh
+++ acts/rl/Gaussian.sh
@@ -1,19 +1,11 @@
 #!/bin/bash
-#SBATCH -J rl_agent        
-#SBATCH -A acf-utk0022         
-#SBATCH --nodes=1              
-#SBATCH --ntasks=1             
-#SBATCH --cpus-per-task=16     
-#SBATCH --time=10:00:00        
-#SBATCH --partition=condo-kvogiatz
-#SBATCH --mem=16G              
-#SBATCH -e gapjob.e%j        
-#SBATCH -o gapjob.o%j        
-#SBATCH --qos=condo-kvogiatz
-
-#SBATCH --mail-user=usuresh3@vols.utk.edu
-#SBATCH --mail-type=ALL
-
-### GAUSSIAN ###
+#SBATCH -J berny
+#SBATCH -A {your_account}
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --time=03:00:00
+#SBATCH --partition={your_partition}
+#SBATCH --qos={your_qos}
 
 python -u run.py
